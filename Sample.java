568650563af200149994b64d40c0eec3c126c0ad
d3athGunn <92803965+d3athGunn@users.noreply.github.com>
2021-10-23 23:34:13 +0530

    Intial Commit

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+
+public static void main(String[] args){
+System.out.println("Hello World");
+System.out.println("Hello Mithra");
+  System.out.println("Hello Hari");
+  System.out.println("Hello Vishnu priya");
+}
+}
+
