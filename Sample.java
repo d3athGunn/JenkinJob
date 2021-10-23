5918ee92bd47f7b39935b1cd76d03d5728c6ea53
d3athGunn <92803965+d3athGunn@users.noreply.github.com>
2021-10-23 23:35:49 +0530

    Second changes

--- Sample.java
+++ Sample.java
@@ -3,7 +3,7 @@ public class HelloWorld{
 public static void main(String[] args){
 System.out.println("Hello World");
 System.out.println("Hello Mithra");
-  System.out.println("Hello Hari");
+  System.out.println("Hello Harikrishnan");
   System.out.println("Hello Vishnu priya");
 }
 }
