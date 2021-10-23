597f73ac6ba06b35751d30c03ef1899c19c8b888
d3athGunn <92803965+d3athGunn@users.noreply.github.com>
2021-10-23 23:40:21 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 
 public static void main(String[] args){
-System.out.println("Hello World");
+System.out.println(" World");
 System.out.println("Hello Mithra");
   System.out.println("Hello Harikrishnan");
   System.out.println("Hpriya");
